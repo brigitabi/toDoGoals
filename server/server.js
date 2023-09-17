7644abe5d41cbd8cd7d462db4f3067e6075647bd
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-09-17 17:32:51 +0200

    Add endpoint test

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ const jwt = require('jsonwebtoken')
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send("App working")
+})
+
 app.get('/todos/:userEmail', async (req, res) => {
     const { userEmail } = req.params
     console.log(userEmail)
